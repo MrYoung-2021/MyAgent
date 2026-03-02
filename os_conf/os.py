b47a6367b0f2586d43c3f25c8c4315b931bf39b6
MrYoung-2021 <112942505+MrYoung-2021@users.noreply.github.com>
2026-03-02 17:10:53 +0800

    del api_key

--- os_conf/os.py
+++ os_conf/os.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["DASHSCOPE_API_KEY"] = "sk-597dad8574134de6be60dbf74113d5de"
+os.environ["DASHSCOPE_API_KEY"] = ""
 
-os.environ["OPENAI_API_KEY"] = "sk-597dad8574134de6be60dbf74113d5de"
+os.environ["OPENAI_API_KEY"] = ""
